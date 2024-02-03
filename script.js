ee54708270c64d9604b7e1937e9c4e45bd4fcdb9
Winston Nichols <157043193+winstonnichols@users.noreply.github.com>
2024-02-03 00:53:03 +0000

    Lab 4 completed (Extra Credit)

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
+var x, w;
+
 function setup() {
   createCanvas(320, 240);
+  x = width / 2;
+  w = 100;
 }
 
 function draw() {
-  background(200);
+  background(0);
   
   // check to condition of the mouse
   if(mouseIsPressed){ // true or false?
     // if true, change the fill color here
-    
-  }
+    fill(100, 175, 225);
+    x = width / 3;
+    w = height / 4;
+  } 
   else { 
     // otherwise do this:
-    
+    fill(55, 200, 65);
+    x = width / 2;
+    w = 100;
   }
-  ellipse(width/2, height/2, 100, 100);
+  ellipse(x, height / 2, w, 100);
 }
